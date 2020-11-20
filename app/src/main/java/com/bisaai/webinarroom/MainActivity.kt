01eef3f89173d958b41b7778e27dde0b36f9e1c2
irfan <irfanfadhilah62@gmail.com>
2020-11-20 18:48:21 +0700

    Initial commit

--- app/src/main/java/com/bisaai/webinarroom/MainActivity.kt
+++ app/src/main/java/com/bisaai/webinarroom/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.bisaai.webinarroom
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
